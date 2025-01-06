f84994b1cba19b802d87147bdd5171cf7a402217
ConorBuffey <c.buffey3244@student.leedsbeckett.ac.uk>
2025-01-06 04:01:25 +0000

    Partley done

--- Game Dev 3 - Week 9/Assets/_Scripts/Player/Player_Bullet.cs
+++ Game Dev 3 - Week 9/Assets/_Scripts/Player/Player_Bullet.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GameDevWithMarco.DesignPattern;
 
-public class Player_Bullet : MonoBehaviour
+namespace GameDevWithMarco.Player
 {
-    [SerializeField] GameObject flash;
-    private void OnCollisionEnter2D(Collision2D collision)
+    public class Player_Bullet : MonoBehaviour
     {
-        //To get where specifically I have collided 
-        foreach (ContactPoint2D contact in collision.contacts)
+        private void OnCollisionEnter2D(Collision2D collision)
         {
-            Vector2 collisionPoint = contact.point;//Add a cool muzzleflash in collision
-            float randomRange = Random.Range(05f, 1.5f);
-            var flashObj = Instantiate(flash, collisionPoint, Quaternion.identity);
-            flashObj.transform.localScale = new Vector3(randomRange, randomRange, randomRange);
-            Destroy(flashObj, 0.5f);
+            //To get where specifically I have collided 
+            foreach (ContactPoint2D contact in collision.contacts)
+            {
+                Vector2 collisionPoint = contact.point;//Add a cool muzzleflash in collision
+                float randomRange = Random.Range(05f, 1.5f);
+                var flashObj = ObjectPoolingPattern.Instance.GetPoolItem(ObjectPoolingPattern.TypeOfPool.MuzzleFlash);
+                flashObj.transform.localScale = new Vector3(randomRange, randomRange, randomRange);
+            }
+            //Returns the game object to the available pool
+            gameObject.SetActive(false);
         }
-        //Returns the game object to the available pool
-        gameObject.SetActive(false);
     }
 }
--- Game Dev 3 - Week 9/Assets/_Scripts/Player/Player_Shooting.cs
+++ Game Dev 3 - Week 9/Assets/_Scripts/Player/Player_Shooting.cs
@@ -15,7 +15,6 @@ namespace GameDevWithMarco.Player
         [SerializeField] GameEvent bulletShot;
         [SerializeField] GameObject muzzleFlash;
         [SerializeField] ParticleSystem sparks;
-        [SerializeField] GameObject bulletPrefab;
 
         private void Start()
         {
@@ -34,7 +33,7 @@ namespace GameDevWithMarco.Player
         void Fire()
         {
             //Spawns the bullet
-            GameObject spawnedBullet = Instantiate(bulletPrefab);
+            GameObject spawnedBullet = ObjectPoolingPattern.Instance.GetPoolItem(ObjectPoolingPattern.TypeOfPool.BulletPool);
 
             //Make the bullet be in the right position
             if (spawnedBullet != null)
@@ -58,12 +57,9 @@ namespace GameDevWithMarco.Player
             CameraRippleEffect.Instance.Ripple(tipOfTheBarrel.transform.position);
 
             //Muzzle flash code
-            var muzzleFlashObject = Instantiate(muzzleFlash, tipOfTheBarrel.transform.position, Quaternion.identity);
+            var muzzleFlashObject = ObjectPoolingPattern.Instance.GetPoolItem(ObjectPoolingPattern.TypeOfPool.MuzzleFlash);
             float randomValue = Random.Range(0.8f, 1.25f);
             muzzleFlash.transform.localScale = new Vector3(randomValue, randomValue, randomValue);
-            //Destroys the muzzleflash and then the bullet
-            Destroy(muzzleFlashObject, 1f);
-            Destroy(spawnedBullet, 1f);
 
             //Plays the sparks particles
             sparks.Play();
